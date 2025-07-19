e64d71568933e0981775036994df12ec75cafa8f
Sunehra Tabassum <sunehrat815@gmail.com>
2025-07-19 20:56:59 +0500

    Build Game Area

---
+++ Tetris Game/src/components/gameElements/gamearea.tsx
@@ -0,0 +1,26 @@
+function Gamearea() {
+  const rows: number = 8;
+  const coloumns: number = 8;
+
+  return (
+    <>
+      <div className='bg-purple-600 p-2 w-fit rounded m-auto mt-6'>
+        {Array.from({ length: rows }).map((_, rowIndex) => {
+          return (
+            <div key={rowIndex} className='flex'>
+              {Array.from({ length: coloumns }).map((_, colIndex) => {
+                return (
+                  <div
+                    key={colIndex}
+                    className='w-14 h-14 border border-white/30'></div>
+                );
+              })}
+            </div>
+          );
+        })}
+      </div>
+    </>
+  );
+}
+
+export default Gamearea;
